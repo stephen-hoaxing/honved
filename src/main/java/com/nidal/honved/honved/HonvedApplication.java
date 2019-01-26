87406454df8c0e6ba2a160993d175d90b87484c6
stephen-hoaxing <neville.rockbottom@gmail.com>
2019-01-26 23:30:12 +0100

    enter

---
+++ src/main/java/com/nidal/honved/honved/HonvedApplication.java
@@ -0,0 +1,14 @@
+package com.nidal.honved.honved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonvedApplication.class, args);
+	}
+
+}
+
